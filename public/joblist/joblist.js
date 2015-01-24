ae7216ecf665121ec50f0897a2dae2ee5e4a628a
Michael Park <mp__@hotmail.com>
2015-01-24 12:47:37 -0800

    0.024h new tel filter

--- public/joblist/joblist.js
+++ public/joblist/joblist.js
@@ -237,48 +237,21 @@ mockupApp.controller( 'SortableTableCtrl', function ( $scope, sortPredicateFacto
 	}
 });
 
-mockupApp.filter('tel', function () { // stolen from the interwebz
+mockupApp.filter('tel', function () {
     return function (tel) {
         if (!tel) { return ''; }
 
-        var value = tel.toString().trim().replace(/^\+/, '');
-
-        if (value.match(/[^0-9]/)) {
-            return tel;
-        }
-
-        var country, city, number;
-
-        switch (value.length) {
-            case 10: // +1PPP####### -> C (PPP) ###-####
-                country = 1;
-                city = value.slice(0, 3);
-                number = value.slice(3);
-                break;
-
-            case 11: // +CPPP####### -> CCC (PP) ###-####
-                country = value[0];
-                city = value.slice(1, 4);
-                number = value.slice(4);
-                break;
-
-            case 12: // +CCCPP####### -> CCC (PP) ###-####
-                country = value.slice(0, 3);
-                city = value.slice(3, 5);
-                number = value.slice(5);
-                break;
-
-            default:
-                return tel;
-        }
-
-        if (country == 1) {
-            country = "";
-        }
-
-        number = number.slice(0, 3) + '-' + number.slice(3);
-
-        return (country + " (" + city + ") " + number).trim();
+		tel = tel.toString().trim();
+		var newtel;
+		newtel = tel.replace( /1(\d{3})(\d{3})(\d{4})\D*/, '1-$1-$2-$3' );
+		if (tel !== newtel) return newtel;
+		newtel = tel.replace( /(\d{3})(\d{3})(\d{4})\D*/, '$1-$2-$3' );
+		if (tel !== newtel) return newtel;
+		newtel = tel.replace( /(\d{3}) (\d{3})(\d{4})\D*/, '$1-$2-$3' );
+		if (tel !== newtel) return newtel;
+		newtel = tel.replace( /(\d{3}) (\d{3})-(\d{4})\D*/, '$1-$2-$3' );
+		if (tel !== newtel) return newtel;
+		return tel;
     };
 });
 
