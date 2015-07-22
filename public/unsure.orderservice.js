d3b43599d9416c5760381de62c8ca50849fe199e
Michael <mp__@hotmail.com>
2015-07-21 17:43:17 -0700

    tech and all views

--- public/unsure.orderservice.js
+++ public/unsure.orderservice.js
@@ -8,12 +8,11 @@ unsureApp.factory( 'orderService', function ( $resource, resourceFactory ) {
 		jobslistView: null,
 		unchanged: true
 	};
-	var orders;
 	var ordersResource = resourceFactory.ordersResource;
 	var customersResource = resourceFactory.customersResource;
 	
 	function getUserOrders() {
-		orders = ordersResource.query( { user: data.currentView }, function(){
+		var orders = ordersResource.query( { user: data.currentView }, function(){
 			for (var i=0; i<orders.length; ++i) {	// compute order age
 				var order = orders[i];
 				order.age = (new Date() - new Date(order.modifiedDate)) / (24 * 60 * 60000); // days
@@ -36,26 +35,45 @@ unsureApp.factory( 'orderService', function ( $resource, resourceFactory ) {
 					});
 				}());
 			}
-
-/*
-			if ( $scope.data.currentOrderNo ) {
-				$scope.data.currentOrder = new $scope.ordersResource();
-				$scope.data.currentOrder.$get( { orderNo: $scope.data.currentOrderNo } ).
-				then( function () {
-					if ( angular.isDefined( $scope.data.currentOrder.orderNo ) ) {
-						var c = new $scope.customersResource();
-						c.$get( { custNo: $scope.data.currentOrder.custNo } ).then( function () {
-							$scope.data.currentOrder.custName = makeDisplayName( c );
-							$scope.data.currentOrder.phone1 = c.phone1;
-							$scope.data.currentOrder.phone2 = c.phone2;
-							$scope.data.currentOrderCopy = angular.copy( $scope.data.currentOrder );	// currentOrderCopy is a *copy* of an order. Edits are done on this copy.
-						});
-					} else {
-						$scope.data.currentOrderNo = null;
-					}
-				});
+		});
+	}
+	getAllOrders = function () {
+		var orders = ordersResource.query( { user: data.currentView }, function(){
+			for (var i=0; i<orders.length; ++i) {	// compute order age
+				var order = orders[i];
+				order.age = (new Date() - new Date(order.modifiedDate)) / (24 * 60 * 60000); // days
 			}
-*/			
+			data.allOrders = [];
+			for ( var i=0; i<orders.length; ++i ) {
+				data.allOrders.push( orders[i] );
+				(function () {
+					var index = i;
+					var c = new customersResource();
+					c.$get( { custNo: orders[index].custNo } ).then( function () {
+						orders[index].custName = makeDisplayName( c );
+						orders[index].phone1 = c.phone1;
+						orders[index].phone2 = c.phone2;
+					});
+				}());
+			}
+		});
+	}
+	var getTechOrders = function () {
+		var orders = ordersResource.query( { user: 'Techs' }, function(){
+			data.ordersOnBench = [];
+			for (var i=0; i<orders.length; ++i) {
+				var order = orders[i];
+				order.age = (new Date() - new Date(order.modifiedDate)) / (24 * 60 * 60000); // days
+				data.ordersOnBench.push(order);
+			}
+		});
+		var orders2 = ordersResource.query( { user: 'Front' }, function(){
+			data.ordersOther = [];
+			for (var i=0; i<orders2.length; ++i) {
+				var order = orders2[i];
+				order.age = (new Date() - new Date(order.modifiedDate)) / (24 * 60 * 60000); // days
+				data.ordersOther.push(order);
+			}
 		});
 	}
 	function makeDisplayName(c) {
